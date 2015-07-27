4cf1ea3bf125abeb077ba56ae8721fa703846ae7
jeffmc <jeffmc@espresso.crunchy.lab>
2015-07-27 08:33:40 -0400

    removed older v2 user interface

--- images/cpm/www/v2/js/roles.js
+++
@@ -1,9 +0,0 @@
-// create the module and name it cpmApp
-	var roleApp = angular.module('RoleApp', ['ngRoute']);
-
-
-	roleApp.controller('roleController', function($scope, $http) {
-		$scope.hithere = 'fromrolecontroller';
-		//console.log('hi from roleController');
-	});
-
