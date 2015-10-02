e45bd18a8c8995a46b77b3d34b43ce592fbbf4f7
jmccormick2001 <jeff.mccormick@crunchydata.com>
2015-10-02 08:26:32 -0500

    backend of backrest restore job working

--- sbin/setupssh.sh
+++ sbin/setupssh.sh
@@ -6,11 +6,8 @@ set ip [lindex $argv 0]
 set user [lindex $argv 1]
 set password [lindex $argv 2]
 
-spawn ssh "$user\@$ip"
-expect "password:"
-send "$password\r";
+spawn ssh -o StrictHostKeyChecking=no "$user\@$ip"
 expect "Last Login:"
-send "hostname\r"
 send "exit\r"
 interact
 
--- sbin/start-restore.sh
+++ sbin/start-restore.sh
@@ -10,6 +10,9 @@
 #	BACKUP_USER - the pg_backrest user to connect to the backup host
 #	BACKUP_SET - the pg_backrest backup set to restore
 #	BACKREST_KEY_PASS - the pg_backrest ssh key passphrase
+#	BACKUP_SERVERNAME - the CPM server name the restore is executing on
+#	BACKUP_SERVERIP - the CPM server ip address the restore is executing on
+#	NODE_PROFILENAME - should be a static value of "backrest-restore"
 #
 # Mounted paths:
 # 	/pgdata - the db path to restore files to
@@ -17,6 +20,21 @@
 #
 
 source /var/cpm/bin/setenv.sh
+
+echo ***Environment vars***
+echo NODE_NAME=$NODE_NAME
+echo REPO_REMOTE_PATH=$REPO_REMOTE_PATH
+echo BACKUP_HOST=$BACKUP_HOST
+echo BACKUP_USER=$BACKUP_USER
+echo BACKUP_SET=$BACKUP_SET
+echo BACKREST_KEY_PASS=$BACKREST_KEY_PASS
+echo BACKUP_SERVERNAME=$BACKUP_SERVERNAME
+echo BACKUP_SERVERIP=$BACKUP_SERVERIP
+echo NODE_PROFILENAME=$NODE_PROFILENAME
+echo BACKUP_SCHEDULEID=$BACKUP_SCHEDULEID
+echo PGDATA=$PGDATA
+echo ***end of Environment vars***
+
 
 #
 # function to create the backrest config file
@@ -26,25 +44,21 @@ sed "s+REPO_REMOTE_PATH+$REPO_REMOTE_PATH+g; \
 	s/BACKUP_HOST/$BACKUP_HOST/g; \
 	s+PGDATA+$PGDATA+g; \
 	s/BACKUP_USER/$BACKUP_USER/g" \
-	pg_backrest.conf.template  > pg_backrest.conf
+	/var/cpm/conf/pg_backrest.conf.template  > /etc/pg_backrest.conf
 }
-
-# PASS IN name of container we will provision
-NODE_NAME=restored
-
-# PASS IN pg_backrest env vars
-REPO_REMOTE_PATH=/tmp/backrest-repo
-BACKUP_HOST=192.168.0.106
-BACKUP_USER=backrest
-BACKUP_SET=latest
 
 # ssh public key to use for backrest..../tmp/keys is mounted
 # copy the private and public keys to the user's directory and set perms
-BACKREST_KEYS=/keys
 
-# password for backrest user..you have to use this to get
-# no-password ssh login setup
-BACKREST_KEY_PASS=backrest
+mkdir /root/.ssh
+cp /keys/id* /root/.ssh/
+chown -R root:root /root/.ssh
+chmod 700 /root/.ssh
+chmod 400 /root/.ssh/id*
+
+# create the local backrest repo which is specified in the 
+# local backrest config file
+mkdir /tmp/backrest-repo
 
 # location to restore database to.../pgdata is mounted
 DB_PATH=$PGDATA
@@ -54,13 +68,15 @@ createBackrestConfig
 
 # test ssh
 #ssh -i /keys/id_rsa -o StrictHostKeyChecking=no $BACKUP_USER@$BACKUP_HOST
-ssh -i /keys/id_rsa -o StrictHostKeyChecking=no $BACKUP_USER@$BACKUP_HOST 'hostname'
-#setupssh.sh $BACKUP_HOST $BACKUP_USER $BACKREST_KEY_PASS
+#ssh -i /keys/id_rsa -o StrictHostKeyChecking=no $BACKUP_USER@$BACKUP_HOST 'hostname'
+setupssh.sh $BACKUP_HOST $BACKUP_USER $BACKREST_KEY_PASS
 
 # run restore daemon as postgres user
 # daemon needs to run 'pg_backrest restore --stanza=main --set=$BACKUP_SET'
 # and send back stats to cpm-admin database
 # and provision a cpm-node
+
+backrestrestore
 
 dummyserver
 
