ff82f7dd5799c23687e7790356d0aa7e57d62353
jmccormick2001 <jeff.mccormick@crunchydata.com>
2015-12-14 09:48:45 -0600

    update docs

--- docs/htmldoc/build.sh
+++ docs/htmldoc/build.sh
@@ -7,3 +7,12 @@ asciidoc \
 -a toc-placement=right \
 -a theme=cerulean \
 ./index.asciidoc
+
+asciidoc \
+-b bootstrap \
+-f ./demo.conf \
+-o rest-api.html \
+-a toc2 \
+-a toc-placement=right \
+-a theme=cerulean \
+./rest-api.asciidoc
