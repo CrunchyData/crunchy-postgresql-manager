295f9ebdfd6d8e6c16f1368b0321103595d9d4e9
jmccormick2001 <jeff.mccormick@crunchydata.com>
2016-01-07 13:10:26 -0600

    update test for swarmapi

--- swarmapi/swarm_test.go
+++ swarmapi/swarm_test.go
@@ -5,21 +5,7 @@ import (
 	"testing"
 )
 
-func TestInspect(t *testing.T) {
-	request := new(DockerInspectRequest)
-	request.ContainerName = "cpm-admin"
-
-	var response DockerInspectResponse
-	var err error
-
-	response, err = DockerInspect(request)
-	if err != nil {
-		//t.Error(err.Error())
-		fmt.Println(err.Error())
-		t.Fail()
-	}
-	fmt.Println(response.IPAddress)
-}
+var TestContainerName = "dfw1"
 
 func TestInfo(t *testing.T) {
 
@@ -65,13 +51,35 @@ func TestPs(t *testing.T) {
 	}
 }
 
+func TestRun(t *testing.T) {
+
+	request := new(DockerRunRequest)
+	var response DockerRunResponse
+	var err error
+
+	request.ContainerName = TestContainerName
+	request.PGDataPath = "/tmp/" + TestContainerName
+	request.Profile = "SM"
+	request.Image = "cpm-node"
+	request.EnvVars = make(map[string]string)
+	request.CPU = "0"
+	request.MEM = "0"
+	response, err = DockerRun(request)
+	if err != nil {
+		//t.Error(err.Error())
+		fmt.Println(err.Error())
+		t.Fail()
+	}
+	fmt.Println(response.ID)
+}
+
 func TestStop(t *testing.T) {
 
 	var response DockerStopResponse
 	var err error
 
 	request := new(DockerStopRequest)
-	request.ContainerName = "fookjadfa"
+	request.ContainerName = TestContainerName
 	response, err = DockerStop(request)
 	if err != nil {
 		//t.Error(err.Error())
@@ -79,4 +87,51 @@ func TestStop(t *testing.T) {
 		t.Fail()
 	}
 	fmt.Println(response.Output)
+}
+
+func TestStart(t *testing.T) {
+
+	request := new(DockerStartRequest)
+	var response DockerStartResponse
+	var err error
+
+	request.ContainerName = TestContainerName
+	response, err = DockerStart(request)
+	if err != nil {
+		//t.Error(err.Error())
+		fmt.Println(err.Error())
+		t.Fail()
+	}
+	fmt.Println(response.Output)
+}
+
+func TestInspect(t *testing.T) {
+	request := new(DockerInspectRequest)
+	request.ContainerName = TestContainerName
+
+	var response DockerInspectResponse
+	var err error
+
+	response, err = DockerInspect(request)
+	if err != nil {
+		//t.Error(err.Error())
+		fmt.Println(err.Error())
+		t.Fail()
+	}
+	fmt.Println(response.IPAddress)
+}
+
+func TestRemove(t *testing.T) {
+	TestStop(t)
+	request := new(DockerRemoveRequest)
+	request.ContainerName = TestContainerName
+
+	var err error
+
+	_, err = DockerRemove(request)
+	if err != nil {
+		//t.Error(err.Error())
+		fmt.Println(err.Error())
+		t.Fail()
+	}
 }
